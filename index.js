2a182a7de670c422861f88e07f8b194a64e2bc91
Programming Hero <programming.hero1@gmail.com>
2022-11-14 23:02:57 -0600

    explore mongodb aggregate and pipleine

--- index.js
+++ index.js
@@ -40,6 +40,51 @@ async function run() {
             res.send(options);
         });
 
+        app.get('/v2/appointmentOptions', async(req, res) =>{
+            const date = req.query.date;
+            const options = await appointmentOptionCollection.aggregate([
+                {
+                    $lookup:{
+                        from: 'bookings',
+                        localField: 'name',
+                        foreignField: 'treatment',
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: { 
+                                        $eq: ['$appointmentDate', date]
+                                     }
+                                }
+                            }
+                         ],
+                        as: 'booked'
+                    }
+                },
+                {
+                   $project: {
+                        name: 1,
+                        slots: 1,
+                        booked: {
+                            $map: {
+                                input: '$booked',
+                                as: 'book',
+                                in: '$$book.slot'
+                            }
+                        }
+                   } 
+                }, 
+                {
+                    $project: {
+                        name: 1,
+                        slots: {
+                            $setDifference: ['$slots', '$booked']
+                        }
+                    }
+                }
+            ]).toArray();
+            res.send(options);
+        })
+
         /***
          * API Naming Convention 
          * app.get('/bookings')
