8157a1c18841c85fcf98418d5eaf039c718bee64
Programming Hero <programming.hero1@gmail.com>
2022-11-14 18:24:18 -0600

    simple server mongodb api for appointment options

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 // middleware
-app.use(cors());0
+app.use(cors());
 app.use(express.json());
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+    try{
+        const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
+        
+        app.get('/appointmentOptions', async(req, res) => {
+            const query = {};
+            const options = await appointmentOptionCollection.find(query).toArray();
+            res.send(options);
+        })
 
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
 
 app.get('/', async(req, res) =>{
     res.send('doctors portal server is running');
