bf52b5f04f3e1d73ac6b5b52652006857233d256
Programming Hero <programming.hero1@gmail.com>
2022-11-15 09:56:00 -0600

    limit appointments for users

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ async function run() {
             const date = req.query.date;
             const query = {};
             const options = await appointmentOptionCollection.find(query).toArray();
-            
+
             // get the bookings of the provided date
             const bookingQuery = { appointmentDate: date }
             const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
-            
+
             // code carefully :D
             options.forEach(option => {
                 const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
@@ -40,28 +40,28 @@ async function run() {
             res.send(options);
         });
 
-        app.get('/v2/appointmentOptions', async(req, res) =>{
+        app.get('/v2/appointmentOptions', async (req, res) => {
             const date = req.query.date;
             const options = await appointmentOptionCollection.aggregate([
                 {
-                    $lookup:{
+                    $lookup: {
                         from: 'bookings',
                         localField: 'name',
                         foreignField: 'treatment',
                         pipeline: [
                             {
                                 $match: {
-                                    $expr: { 
+                                    $expr: {
                                         $eq: ['$appointmentDate', date]
-                                     }
+                                    }
                                 }
                             }
-                         ],
+                        ],
                         as: 'booked'
                     }
                 },
                 {
-                   $project: {
+                    $project: {
                         name: 1,
                         slots: 1,
                         booked: {
@@ -71,8 +71,8 @@ async function run() {
                                 in: '$$book.slot'
                             }
                         }
-                   } 
-                }, 
+                    }
+                },
                 {
                     $project: {
                         name: 1,
@@ -96,6 +96,20 @@ async function run() {
 
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
+            console.log(booking);
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                email: booking.email,
+                treatment: booking.treatment 
+            }
+
+            const alreadyBooked = await bookingsCollection.find(query).toArray();
+
+            if (alreadyBooked.length){
+                const message = `You already have a booking on ${booking.appointmentDate}`
+                return res.send({acknowledged: false, message})
+            }
+
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         })
